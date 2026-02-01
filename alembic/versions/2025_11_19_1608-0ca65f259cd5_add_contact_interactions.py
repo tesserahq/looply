5ecb8106f1d5aa7d7187cc367436dd7a895c4b06
Emiliano Jankowski <ejankowski@gmail.com>
2026-02-01 23:29:59 +0100

    updating libs and fmt

--- alembic/versions/2025_11_19_1608-0ca65f259cd5_add_contact_interactions.py
+++ alembic/versions/2025_11_19_1608-0ca65f259cd5_add_contact_interactions.py
@@ -11,7 +11,6 @@ from typing import Sequence, Union
 from alembic import op
 import sqlalchemy as sa
 
-
 # revision identifiers, used by Alembic.
 revision: str = "0ca65f259cd5"
 down_revision: Union[str, None] = "initialize_database"
